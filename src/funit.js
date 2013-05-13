1820a8c4011c224745cdbedd3e69e8ee57fc08cd
murer <murer@dextra-sw.com>
2013-05-13 16:45:25 -0300

    test

--- src/funit.js
+++ src/funit.js
@@ -1,14 +1,32 @@
-(function(t, $) {
+(function(QUnit, $) {
+
+	function FUnit() {
+
+	}
+
+	function extend(target) {
+		if (arguments.length >= 2) {
+			for ( var i = 1; i < arguments.length; i++) {
+				var arg = arguments[i];
+				if(arg) {
+					for(var k in arg) {
+						target[k] = arg[k];
+					}
+				}
+			}
+		}
+		return target;
+	}
 
 	function waitFor(wait) {
-		
-		if(typeof(wait) == 'function') {
-			return waitFor([wait]);
+
+		if (typeof (wait) == 'function') {
+			return waitFor([ wait ]);
 		}
-		
+
 		function check(entry) {
-			for(var i = 0; i < entry.wait.length; i++) {
-				if(!entry.wait[i]()) {
+			for ( var i = 0; i < entry.wait.length; i++) {
+				if (!entry.wait[i]()) {
 					return false;
 				}
 			}
@@ -19,7 +37,7 @@
 			var time = entry._time - (new Date().getTime() - entry.init);
 			if (time >= 0 && !check(entry)) {
 				if (!entry.stopped) {
-					t.stop();
+					QUnit.stop();
 					entry.stopped = true;
 				}
 				setTimeout(function() {
@@ -28,9 +46,9 @@
 				return;
 			}
 			if (entry.stopped) {
-				t.start();
+				QUnit.start();
 			}
-			if(time < 0) {
+			if (time < 0) {
 				entry._timeout();
 				return;
 			}
@@ -38,135 +56,167 @@
 		}
 
 		return {
-			wait : wait,
-			done : function(callback) {
-				this._done = callback;
-				this.init = new Date().getTime();
-				this._timeout = this._timeout || function() {
-					throw 'timeout';
-				}
-				this._time = this._time || 5000;
-				makeWait(this);
-				return this;
-			},
-			timeout : function(callback, time) {
-				if(typeof(callback) == 'number') {
-					time = callback;
-					callback = null;
-				}
-				this._timeout = callback;
-				this._time = time;
-				return this;
-			}
+		    wait : wait,
+		    done : function(callback) {
+			    this._done = callback;
+			    this.init = new Date().getTime();
+			    this._timeout = this._timeout || function() {
+				    throw 'timeout';
+			    }
+			    this._time = this._time || 5000;
+			    makeWait(this);
+			    return this;
+		    },
+		    timeout : function(callback, time) {
+			    if (typeof (callback) == 'number') {
+				    time = callback;
+				    callback = null;
+			    }
+			    this._timeout = callback;
+			    this._time = time;
+			    return this;
+		    }
 		};
 	}
 
-//	function toArray(array) {
-//		var ret = [];
-//		for ( var i = 0; i < array.length; i++) {
-//			ret[i] = array[i];
-//		}
-//		return ret;
-//	}
-//
-//	function prepareOpen(url) {
-//		return function(finish) {
-//			$('#frame').attr('src', url);
-//			waitFor(function() {
-//				var frame = $('#frame');
-//				return frame.length && frame[0].contentWindow
-//						&& frame[0].contentWindow.jQuery;
-//			}, function() {
-//				makeCall(function(w, $) {
-//					$(w.document).click(function(evt) {
-//						var target = $(evt.target);
-//						if (target.is('a[href]')) {
-//							var href = target.attr('href');
-//							w.location = '' + w.location.pathname + href;
-//						}
-//					});
-//					finish();
-//				});
-//			});
-//		}
-//	}
-//
-//	function convertWait(p) {
-//		if (typeof (p) == 'string') {
-//			return function() {
-//				var frame = $('#frame');
-//				return frame.length && frame[0].contentWindow
-//						&& frame[0].contentWindow.jQuery
-//						&& frame[0].contentWindow.jQuery(p).length;
-//			}
-//		}
-//		return p;
-//	}
-//
-//	function makeCall(f) {
-//		var _window = $("#frame")[0].contentWindow;
-//		var _$ = _window.jQuery;
-//		f.call(_window, _window, _$);
-//	}
-//
-//	function prepareStep(args) {
-//		return function(finish) {
-//			var w = null;
-//			var f = null;
-//			if (args.length == 1 && args[0].call) {
-//				f = args[0];
-//			}
-//			if (args.length == 2) {
-//				w = args[0];
-//				f = args[1];
-//			}
-//			if (!w) {
-//				makeCall(f);
-//				finish();
-//				return;
-//			}
-//			w = convertWait(w);
-//			waitFor(w, function() {
-//				makeCall(f);
-//				finish();
-//			});
-//		}
-//	}
-//
-//	function prepareSteps(steps, fn) {
-//		fn(function(url) {
-//			steps.push(prepareOpen(url));
-//		}, function() {
-//			steps.push(prepareStep(toArray(arguments)));
-//		});
-//		return steps;
-//	}
-//
-//	function consume(steps) {
-//		if (!steps.length) {
-//			return;
-//		}
-//		var step = steps.shift();
-//		step(function() {
-//			consume(steps);
-//		});
-//	}
-//
-//	function createTest(steps) {
-//		$('#qunit-fixture').html('<iframe id="frame"></iframe>');
-//		consume(steps);
-//	}
-//
-//	function pageTest(name, fn) {
-//		var steps = prepareSteps([], fn);
-//		test(name, function() {
-//			createTest(steps)
-//		});
-//	}
-//
-//	t.pageTest = pageTest;
-	t.waitFor = waitFor;
-
-	return t;
+	function Page() {
+		this.steps = [];
+	}
+	extend(Page.prototype, {
+		step : function(name, func) {
+			this.steps.push({ name: name, func: func });
+		}
+	});
 	
+	function executeTest(page) {
+		if(!page.steps.length) {
+			return;	
+		}
+		var step = page.steps.shift();
+		console.info('executing', step.name);
+		step.func(page);
+		setTimeout(function() {
+			QUnit.start();
+			executeTest(page);
+		}, 1);
+		QUnit.stop();
+	}
+	
+	function pageTest(name, func) {
+		QUnit.test(name, function() {
+			var page = new Page();
+			page.name = name;
+			func(page);
+			executeTest(page);
+		});
+	}
+
+	// function toArray(array) {
+	// var ret = [];
+	// for ( var i = 0; i < array.length; i++) {
+	// ret[i] = array[i];
+	// }
+	// return ret;
+	// }
+	//
+	// function prepareOpen(url) {
+	// return function(finish) {
+	// $('#frame').attr('src', url);
+	// waitFor(function() {
+	// var frame = $('#frame');
+	// return frame.length && frame[0].contentWindow
+	// && frame[0].contentWindow.jQuery;
+	// }, function() {
+	// makeCall(function(w, $) {
+	// $(w.document).click(function(evt) {
+	// var target = $(evt.target);
+	// if (target.is('a[href]')) {
+	// var href = target.attr('href');
+	// w.location = '' + w.location.pathname + href;
+	// }
+	// });
+	// finish();
+	// });
+	// });
+	// }
+	// }
+	//
+	// function convertWait(p) {
+	// if (typeof (p) == 'string') {
+	// return function() {
+	// var frame = $('#frame');
+	// return frame.length && frame[0].contentWindow
+	// && frame[0].contentWindow.jQuery
+	// && frame[0].contentWindow.jQuery(p).length;
+	// }
+	// }
+	// return p;
+	// }
+	//
+	// function makeCall(f) {
+	// var _window = $("#frame")[0].contentWindow;
+	// var _$ = _window.jQuery;
+	// f.call(_window, _window, _$);
+	// }
+	//
+	// function prepareStep(args) {
+	// return function(finish) {
+	// var w = null;
+	// var f = null;
+	// if (args.length == 1 && args[0].call) {
+	// f = args[0];
+	// }
+	// if (args.length == 2) {
+	// w = args[0];
+	// f = args[1];
+	// }
+	// if (!w) {
+	// makeCall(f);
+	// finish();
+	// return;
+	// }
+	// w = convertWait(w);
+	// waitFor(w, function() {
+	// makeCall(f);
+	// finish();
+	// });
+	// }
+	// }
+	//
+	// function prepareSteps(steps, fn) {
+	// fn(function(url) {
+	// steps.push(prepareOpen(url));
+	// }, function() {
+	// steps.push(prepareStep(toArray(arguments)));
+	// });
+	// return steps;
+	// }
+	//
+	// function consume(steps) {
+	// if (!steps.length) {
+	// return;
+	// }
+	// var step = steps.shift();
+	// step(function() {
+	// consume(steps);
+	// });
+	// }
+	//
+	// function createTest(steps) {
+	// $('#qunit-fixture').html('<iframe id="frame"></iframe>');
+	// consume(steps);
+	// }
+	//
+	// function pageTest(name, fn) {
+	// var steps = prepareSteps([], fn);
+	// test(name, function() {
+	// createTest(steps)
+	// });
+	// }
+	//
+	QUnit.pageTest = pageTest;
+	QUnit.waitFor = waitFor;
+
+	return QUnit;
+
 })(QUnit, jQuery);
---
+++ test/funit-page-test.js
@@ -0,0 +1,70 @@
+(function(t) {
+
+	t.module('FUnitFace');
+
+	t.pageTest("step flow", function(page) {
+
+		var steps = [];
+
+		page.step('step a', function() {
+			steps.push('a');
+			page.step('step c', function() {
+				steps.push('c');
+				t.deepEqual(steps, [ 'a', 'b', 'c' ]);
+			})
+		});
+
+		page.step('step b', function() {
+			steps.push('b');
+		});
+
+		page.step('step assert', function() {
+			t.deepEqual(steps, [ 'a', 'b' ]);
+		});
+
+	});
+
+	// t.pageTest("face test raw mode", function(face) {
+
+	// function selector(path) {
+	// return function() {
+	// var ret = face.$(path);
+	// return ret.length ? ret : false;
+	// }
+	// }
+
+	// face.step('open url', function() {
+	// face.window.location = 'panel.html';
+	// });
+
+	// face.step([ selector('.ajax'), selector('.result') ]).done(function(link)
+	// {
+	// t.equal(face.$('.result').text(), 'Initial');
+	// link.click();
+	// });
+	//
+	// face.step(selector('.result:contains("Count: 1")')).done(function(result)
+	// {
+	// t.equal(result.text(), 'Count: 1');
+	// });
+
+	// });
+
+	// t.faceTest("face test soft mode", function(face) {
+	//
+	// face.step('open page').open('./panel.html');
+	//
+	// face.step('go ajax').wait([ '.ajax', '.result' ]).done(function(window,
+	// $, link, result) {
+	// t.equal(result.text(), 'Initial');
+	// link.click();
+	// });
+	//
+	// face.step().wait('.result:contains("Count: 1")').done(function(window, $,
+	// result) {
+	// t.equal(result.text(), 'Count: 1');
+	// });
+	//
+	// });
+
+})(QUnit);
