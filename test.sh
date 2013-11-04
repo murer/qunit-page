878754da0a55cfd0c0fd4200ec10cd30a43161ee
murer <murer@dextra-sw.com>
2013-11-04 10:05:34 -0200

    travis

--- test.sh
+++ test.sh
@@ -6,15 +6,20 @@ if ! ./target/lib/phantomjs-1.9.2-linux-i686/bin/phantomjs --version; then
 	mkdir target/lib -p
 	set -e
 	cd target/lib
-	wget https://phantomjs.googlecode.com/files/phantomjs-1.9.2-linux-i686.tar.bz2 -O phantomjs.tar.bz2
-	tar xjf phantomjs.tar.bz2
+	if file /sbin/init | grep "32-bit"; then
+		wget https://phantomjs.googlecode.com/files/phantomjs-1.9.2-linux-i686.tar.bz2 -O phantomjs.tar.bz2
+		tar xjf phantomjs.tar.bz2
+		ln -s phantomjs-1.9.2-linux-i686 phantomjs
+	else
+		wget https://phantomjs.googlecode.com/files/phantomjs-1.9.2-linux-x86_64.tar.bz2
+		tar xjf phantomjs.tar.bz2
+		ln -s phantomjs-1.9.2-linux-x86_64 phantomjs 
+	fi
 	cd -
-	ls ./target/lib/phantomjs-1.9.2-linux-i686/bin/phantomjs
-	file /sbin/init
-	./target/lib/phantomjs-1.9.2-linux-i686/bin/phantomjs --version
+	./target/lib/phantomjs/bin/phantomjs --version
 fi
 
-./target/lib/phantomjs-1.9.2-linux-i686/bin/phantomjs phantomjs-test/run-qunit.js http://localhost:8084/test/test.html
+./target/lib/phantomjs/bin/phantomjs phantomjs-test/run-qunit.js http://localhost:8084/test/test.html
 	
 
 
